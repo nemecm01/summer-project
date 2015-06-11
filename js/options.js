d2e1496943a4a2c22c30e3084883d4f9ed3628b7
Max <threeguysandahobo@gmail.com>
2015-06-11 16:52:39 -0500

    add news sidebar functionality

--- js/options.js
+++ js/options.js
@@ -1,5 +1,28 @@
 function colorChange(){
         $('#optionModalBody').html("");
-        $('#optionModalBody').load('options.js');
+        $('#optionModalBody').load('options.html',function(){
+              jscolor.init();
+            } );
+      
         
 }
+function goNews(){
+        $('#mainPage').html("");
+        $('#mainPage').load('news-page/news.html');
+        
+}
+function goGrades(){
+        $('#mainPage').html("");
+        $('#mainPage').load('grade-page.html');
+        
+}
+
+$("#saveColor").on("click",function(){
+        var answer=confirm("Are you sure you want this color?")
+        if(answer){
+                  $('.sidebar-nav').shadeout({
+                color: '#0D23E8',
+                increment: 10
+            });
+            }
+        })
